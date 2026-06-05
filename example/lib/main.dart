6c04c54ee577d892b0218a706dc51d4a54702679
Maicarons <136282328+Maicarons@users.noreply.github.com>
2026-06-05 12:36:54 +0800

    fix: remove debug banner from example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,6 +14,7 @@ class YOLOExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ultralytics YOLO',
       themeMode: ThemeMode.dark,
       theme: ThemeData.dark(useMaterial3: true),
